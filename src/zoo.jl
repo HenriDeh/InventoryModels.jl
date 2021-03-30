8c840e13ba08f52634a6ffc8be87e9367c3f80bc
Dehaybe <henri.dehaybe@uclouvain.be>
2021-03-30 12:29:35 +0200

    added General 10 zoo model

--- src/zoo.jl
+++ src/zoo.jl
@@ -9,32 +9,37 @@ function sl_sip(h, b, K, CV, c, μ::Vector, LT::Int = 0; lostsales = false)
     InventorySystem(length(μ), bom)
 end
 
-function assembly10(;holding_costs, fixed_costs, variable_costs, leadtimes, backorder_cost, demand_forecast, CV, initlevels = zeros(10))
+"""
+InventoryModels.assembly10(holding_costs = ones(10), fixed_costs = 100*ones(10), variable_costs = zeros(10), leadtimes=ones(10), backorder_cost = 10, demand_forecast = [20,40,60,40,20,40,60], CV = 0.25)
+
+order capacities are decentralized, this does not implements the "shared ressources" generalization of Thevenin et al.
+"""
+function assembly10(;holding_costs, fixed_costs, variable_costs, leadtimes, backorder_cost, demand_forecast, CV, order_capacities = fill(Inf, 10), inventory_capacities = fill(Inf, 10), initlevels = zeros(10))
     suppliers = []
     lts = []
     items = []
     assembs = []
     for i in 5:10
-        sup = Supplier(fixed_costs[i], variable_costs[i])
+        sup = Supplier(fixed_costs[i], variable_costs[i], capacity = order_capacities[i])
         lt = LeadTime(leadtimes[i], 0., sup)
-        item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt)
+        item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt, capacity = inventory_capacities[i])
         push!(suppliers, sup)
         push!(lts, lt)
         push!(items, item)
     end
     sfitems = Iterators.Stateful(items)
     for i in 2:4
-        ass = Assembly(fixed_costs[i], variable_costs[i], popfirst!(sfitems) => 1, popfirst!(sfitems) => 1)
-        lt = LeadTime(holding_costs[i*2+1] + holding_costs[i*2+2], leadtimes[i], 0., ass)
-        item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt)
+        ass = Assembly(fixed_costs[i], variable_costs[i], popfirst!(sfitems) => 1, popfirst!(sfitems) => 1, capacity = order_capacities[i])
+        lt = LeadTime(leadtimes[i], 0., ass)
+        item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt, capacity = inventory_capacities[i])
         push!(assembs, ass)
         push!(lts, lt)
         push!(items, item)
     end
     i = 1
-    ass = Assembly(fixed_costs[i], variable_costs[i], popfirst!(sfitems) => 1, popfirst!(sfitems) => 1, popfirst!(sfitems) => 1)
-    lt = LeadTime(holding_costs[2] + holding_costs[3] + holding_costs[4], leadtimes[i], 0., ass)
-    item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt)
+    ass = Assembly(fixed_costs[i], variable_costs[i], popfirst!(sfitems) => 1, popfirst!(sfitems) => 1, popfirst!(sfitems) => 1, capacity = order_capacities[i])
+    lt = LeadTime(leadtimes[i], 0., ass)
+    item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt, capacity = inventory_capacities[i])
     push!(assembs, ass)
     push!(lts, lt)
     push!(items, item)
@@ -42,3 +47,62 @@ function assembly10(;holding_costs, fixed_costs, variable_costs, leadtimes, back
     market = Market(backorder_cost, CVNormal{CV}, length(demand_forecast), items[1], 0., [demand_forecast; demand_forecast])
     InventorySystem(length(demand_forecast), suppliers..., assembs..., lts..., items..., market)
 end 
+
+
+function general10(;holding_costs, fixed_costs, variable_costs, leadtimes, backorder_costs, demand_forecasts, CVs, order_capacities = fill(Inf, 10), inventory_capacities = fill(Inf, 10), initlevels = zeros(10))
+    suppliers = []
+    lts = []
+    items = []
+    assembs = []
+    markets = []
+    for i in 8:10
+        sup = Supplier(fixed_costs[i], variable_costs[i], capacity = order_capacities[i])
+        lt = LeadTime(leadtimes[i], 0., sup)
+        item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt, capacity = inventory_capacities[i])
+        push!(suppliers, sup)
+        push!(lts, lt)
+        push!(items, item)
+    end
+    for (j, i) in enumerate(5:6)
+        ass = Assembly(fixed_costs[i], variable_costs[i], items[j] => 1, items[j+1] => 1, capacity = order_capacities[i])
+        lt = LeadTime(leadtimes[i], 0., ass)
+        item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt, capacity = inventory_capacities[i])
+        push!(assembs, ass)
+        push!(lts, lt)
+        push!(items, item)
+    end
+    i = 7
+    ass = Assembly(fixed_costs[i], variable_costs[i], items[3] => 1, capacity = order_capacities[i])
+    lt = LeadTime(leadtimes[i], 0., ass)
+    item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt, capacity = inventory_capacities[i])
+    push!(assembs, ass)
+    push!(lts, lt)
+    push!(items, item)
+    i = 1
+    ass = Assembly(fixed_costs[i], variable_costs[i], items[4] => 1, capacity = order_capacities[i])
+    lt = LeadTime(leadtimes[i], 0., ass)
+    item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt, capacity = inventory_capacities[i])
+    push!(assembs, ass)
+    push!(lts, lt)
+    push!(items, item)
+    for i in 2:3
+        ass = Assembly(fixed_costs[i], variable_costs[i], items[i+2] => 1, items[i+3] => 1, capacity = order_capacities[i])
+        lt = LeadTime(leadtimes[i], 0., ass)
+        item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt, capacity = inventory_capacities[i])
+        push!(assembs, ass)
+        push!(lts, lt)
+        push!(items, item)
+    end
+    i = 4
+    ass = Assembly(fixed_costs[i], variable_costs[i], items[6] => 1, capacity = order_capacities[i])
+    lt = LeadTime(leadtimes[i], 0., ass)
+    item = Item(holding_costs[i], sSPolicy(), initlevels[i], lt, capacity = inventory_capacities[i])
+    push!(assembs, ass)
+    push!(lts, lt)
+    push!(items, item)
+    for i in 1:4
+        df = demand_forecasts[i]
+        push!(markets, Market(backorder_costs[i], CVNormal{CVs[i]}, length(df), items[i+6], 0., [df; df]))
+    end
+    InventorySystem(minimum(length.(demand_forecasts)), suppliers..., lts..., assembs..., items..., markets...)
+end
