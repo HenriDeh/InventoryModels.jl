743b7d0fb54f3942b1e7607c755913bbce1cded8
Dehaybe <henri.dehaybe@uclouvain.be>
2021-04-30 18:06:04 +0200

    fix fix

--- src/Scarf/interface.jl
+++ src/Scarf/interface.jl
@@ -5,7 +5,7 @@ function Scarf.Instance(is::InventorySystem, gamma = 1.)
     inv = ep.inventory
     supplier = only(ep.sources)
     market = ep.market
-    leadtime = supplier.leadtime
+    leadtime = supplier.leadtime.leadtime
     Scarf.Instance(inv.holding_cost.h, market.stockout_cost.b, supplier.order_cost.K, 
         supplier.order_cost.c, cv(market.demand_dist), leadtime, rand.(market.forecast_reset[1].itr.xs), gamma, 
         backlog = !market.lostsales)
