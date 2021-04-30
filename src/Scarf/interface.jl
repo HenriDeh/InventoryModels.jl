9725a72dc2a646d75fb0bbc179a7bc69801f6238
Dehaybe <henri.dehaybe@uclouvain.be>
2021-04-30 17:47:56 +0200

    fix interface Scarf

--- src/Scarf/interface.jl
+++ src/Scarf/interface.jl
@@ -1,18 +1,12 @@
 function Scarf.Instance(is::InventorySystem, gamma = 1.)
-    @assert 3 <= length(is.bom) <= 4
-    @assert is.bom[1] isa Market
-    market = is.bom[1]
-    @assert is.bom[2] isa Item
-    item = is.bom[2]
-    @assert is.bom[end] isa Supplier
-    supplier = is.bom[end]
-    if length(is.bom) == 4
-        @assert is.bom[3] isa LeadTime
-        leadtime = is.bom[3].leadtime
-    else
-        leadtime =0
-    end
-    Scarf.Instance(item.holding_cost.h, market.stockout_cost.b, supplier.order_cost.K, 
+    @assert only(is.bom) isa EndProduct
+    @assert only(only(is.bom).sources) isa Supplier
+    ep = only(is.bom)
+    inv = ep.inventory
+    supplier = only(ep.sources)
+    market = ep.market
+    leadtime = supplier.leadtime
+    Scarf.Instance(inv.holding_cost.h, market.stockout_cost.b, supplier.order_cost.K, 
         supplier.order_cost.c, cv(market.demand_dist), leadtime, rand.(market.forecast_reset[1].itr.xs), gamma, 
         backlog = !market.lostsales)
 end
