c735ab5eda112da65d727a74cca5c0cd9fe3d379
Dehaybe <henri.dehaybe@hotmail.be>
2021-06-28 14:00:29 +0200

    fix test assembly

--- test/assembly.jl
+++ test/assembly.jl
@@ -1,11 +1,13 @@
 @testset "assembly.jl" begin
-    comp1 = Inventory(0, 50)
-    comp2 = Inventory(0, 20)
+    comp1 = Item(Inventory(0, 50), Supplier(100, 10))
+    comp2 = Item(Inventory(0, 20), Supplier(100, 10))
     ass = Assembly(100,10, comp1 => 2, comp2 =>1)
     @test state(ass) == []
     @test state_size(ass) == 0
     dest = []
     InventoryModels.pull!(ass, 10, dest)
+    comp1(0)
+    comp2(0)
     InventoryModels.dispatch!(comp1)
     InventoryModels.dispatch!(comp2)
     InventoryModels.dispatch!(ass)
@@ -17,6 +19,8 @@
     @test state(comp1) == [30]
     @test state(comp2) == [10]
     InventoryModels.pull!(ass, 12, dest)
+    comp1(0)
+    comp2(0)
     InventoryModels.dispatch!(comp1)
     InventoryModels.dispatch!(comp2)
     InventoryModels.dispatch!(ass)
@@ -28,13 +32,15 @@
     @test state(comp1) == [10]
     @test state(comp2) == [0]
 
-    comp1 = Inventory(0, 50)
-    comp2 = Inventory(0, 20)
+    comp1 = Item(Inventory(0, 50), Supplier(100, 10))
+    comp2 = Item(Inventory(0, 20), Supplier(100, 10))
     ass = Assembly(100,10, comp1 => 2, comp2 =>1, leadtime = LeadTime(3, 20))
     @test state(ass) == [20,20,20]
     @test state_size(ass) == 3
     dest = []
     InventoryModels.pull!(ass, 10, dest)
+    comp1(0)
+    comp2(0)
     InventoryModels.dispatch!(comp1)
     InventoryModels.dispatch!(comp2)
     InventoryModels.dispatch!(ass)
@@ -46,6 +52,8 @@
     @test state(comp1) == [30]
     @test state(comp2) == [10]
     InventoryModels.pull!(ass, 12, dest)
+    comp1(0)
+    comp2(0)
     InventoryModels.dispatch!(comp1)
     InventoryModels.dispatch!(comp2)
     InventoryModels.dispatch!(ass)
