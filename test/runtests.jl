8dd8e36526fc48373577ca0956f833266649fb05
Dehaybe <henri.dehaybe@hotmail.be>
2021-06-28 15:58:04 +0200

    rename test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
 using Revise, Distributions, InventoryModels, Test
 
 @testset "InventoryModels.jl" begin
-    include("inventory.jl")
-    include("leadtime.jl")
-    include("supplier.jl")
-    include("market.jl")
-    include("assembly.jl")
-    include("item.jl")
-    include("endproduct.jl")
-    include("inventory_system.jl")
-    include("constraints.jl")
+    include("test_inventory.jl")
+    include("test_leadtime.jl")
+    include("test_supplier.jl")
+    include("test_market.jl")
+    include("test_assembly.jl")
+    include("test_item.jl")
+    include("test_endproduct.jl")
+    include("test_inventory_system.jl")
+    include("test_constraints.jl")
 end
