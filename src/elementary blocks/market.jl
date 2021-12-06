baaa8f7da5c437dc6940ec467b5b37d70f595ff7
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2021-12-06 15:27:09 +0100

    fix print_state market

--- src/elementary blocks/market.jl
+++ src/elementary blocks/market.jl
@@ -14,11 +14,10 @@ mutable struct Market{D<:Distribution, L<:Union{Bool, AbstractFloat}, F, Df, Db 
     name::String
 end
 
-function Market(stockout_cost, demand_distribution::Type{<:Distribution}, horizon::Int, backorder_reset::NumDist, forecast_reset::State...; lostsales = false, name="market")
+function Market(stockout_cost, demand_distribution::Type{<:Distribution}, horizon::Int, backorder_reset::NumDist, forecast_reset::State...; lostsales::Bool = false, name="market")
     @assert hasmethod(stockout_cost, Tuple{Market}) "stockout cost must have a method with `(::Market)` signature"
     @assert all(x -> x isa State, forecast_reset)
     @assert length(forecast_reset) == length(params(demand_distribution())) "Please provide a reset distribution for each parameter of $demand_distribution"
-    @assert 0 <= lostsales <= 1
     bd = parametrify(backorder_reset)
     frd = Iterators.Stateful.(cycle.(parametrify.(forecast_reset)))
     forecasts = Float64[rand(param) for _ in 1:horizon for param in popfirst!.(frd)]
@@ -33,7 +32,7 @@ end
 
 state(ma::Market) = [ma.backorder; ma.forecasts]
 state_size(ma::Market) = 1+ma.horizon*length(ma.forecast_reset)
-function print_state(ma::Market)
+function print_state(ma::Market, forecast = true)
     n_param = length(ma.forecasts) ÷ ma.horizon
     forecasts = ["$(ma.name) demand($j) t+$(i-1)" => p for (i,pars) in enumerate(partition(ma.forecasts, n_param)) for (j,p) in enumerate(pars)]
     if !forecast 
@@ -77,6 +76,7 @@ function reset!(ma::Market)
     empty!(ma.fillrate_log)
     return nothing
 end
+
 
 inventory_position(ma::Market) = -ma.backorder
 
