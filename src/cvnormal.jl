8082a2ef1da4c1d55273a10cc94842c1592466cd
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2022-06-01 17:55:46 +0200

    add BoundedWiener

--- src/cvnormal.jl
+++ src/cvnormal.jl
@@ -1,3 +1,5 @@
+export CVNormal, BoundedWienerProcess
+
 struct CVNormal{CV,T} <: ContinuousUnivariateDistribution
     normal::Normal{T}
     CVNormal{CV}(n::Normal{T}) where {CV, T} = new{CV,T}(n)
@@ -15,3 +17,21 @@ cv(d::CVNormal{CV}) where CV = CV
 cv(d::Type{CVNormal{CV}}) where CV = CV
 
 #Base.show(io::IO, n::CVNormal) = show(n.normal)
+
+####BoundedWienerProcess
+mutable struct BoundedWienerProcess{T, D <: ContinuousUnivariateDistribution} <: ContinuousUnivariateDistribution
+    x::T
+    d::D
+    lb::T
+    ub::T
+end
+
+BoundedWienerProcess(d::Distribution, lb, ub) = BoundedWienerProcess(rand(Uniform(lb,ub)), d, eltype(d)(lb), eltype(d)(ub))
+
+function Base.rand(bw::BoundedWienerProcess)
+    delta = rand(bw.d)
+    bw.x = max(bw.lb, min(bw.ub, bw.x + delta))
+    bw.x
+end
+
+reset!(b::BoundedWienerProcess) = b.x = rand(Uniform(b.lb,b.ub))
