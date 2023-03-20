1b201e3c9a92716bc08c2ee44d3524440e978c00
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-03-20 15:09:48 +0100

    refactor zoo

--- src/zoo.jl
+++ src/zoo.jl
@@ -1,6 +1,6 @@
-function sl_sip(h, b, K, CV, c, μ::Vector, start_inventory, LT::Int = 0; lostsales = false, horizon = length(μ), policy = sSPolicy(), periods = horizon)
+function sl_sip(h, b, K, c, μ::Vector, start_inventory, LT::Int = 0; lostsales = false, horizon = length(μ), policy = sSPolicy(), periods = horizon, d_type)
     item = EndProduct(
-        Market(b, CVNormal{CV}, horizon, 0, μ, lostsales = lostsales),
+        Market(b, d_type, horizon, 0, μ, lostsales = lostsales),
         Inventory(h, start_inventory),
         Supplier(K,c, leadtime = LeadTime(LT, 0)),
         policy = policy
@@ -8,9 +8,9 @@ function sl_sip(h, b, K, CV, c, μ::Vector, start_inventory, LT::Int = 0; lostsa
     InventorySystem(periods, [item])
 end
 
-function sl_sip(h, b, K, CV, c, μ::Distribution, start_inventory, LT::Int = 0; lostsales = false, policy = sSPolicy(), horizon::Int, periods::Int)
+function sl_sip(h, b, K, c, μ::Distribution, start_inventory, LT::Int = 0; lostsales = false, policy = sSPolicy(), horizon::Int, periods::Int, d_type)
     item = EndProduct(
-        Market(b, CVNormal{CV}, horizon, 0, μ, lostsales = lostsales),
+        Market(b, d_type, horizon, 0, μ, lostsales = lostsales),
         Inventory(h, start_inventory),
         Supplier(K,c, leadtime = LeadTime(LT, 0)),
         policy = policy
